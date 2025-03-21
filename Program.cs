58c96bfd86c4253ba12f4c8deb5faf2da4fb4d81
beatrizpoffo <beatrizpoffo7a5@gmail.com>
2025-03-21 02:40:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write(" P a l ");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("  m e i ");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Green;
+Console.Write(" r a s ");
+
+Console.ResetColor();
